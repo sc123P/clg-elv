2604828aaf51209b5e8611052d3a738fd0bf83a1
Simon <simon.casorla@outlook.fr>
2024-01-24 10:03:12 -0400

    cleaning up signup function

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -5,15 +5,10 @@ import jwt from "jsonwebtoken";
 export function signup (req, res, next){
 
     //CHECK EXISTING USER
-    // const q = "SELECT * FROM elv.user WHERE email = ? OR username = ? "
     const checkExistingAdminQuery = "SELECT * FROM elv.user WHERE isAdmin = 1";
 
-    // db.query(q,[req.body.email, req.body.username], (err, data)=>{
         db.query(checkExistingAdminQuery, (err, data) => {
-        //bcrypt.hash(req.body.password, 10)
-
         if(err) return res.json(err)
-        // if(data.length) return res.status(409).json("Cet utilisateur existe déjà");
     
         // Si un administrateur existe déjà, empêche la création d'un autre compte
         if (data.length > 0) {
@@ -22,14 +17,6 @@ export function signup (req, res, next){
 
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
-
-        // const q = "INSERT INTO user(`username`, `email`, `password`, `isAdmin`) VALUES (?)"
-        // const values = [
-        //     req.body.username,
-        //     req.body.email,
-        //     hash,
-        //     true,
-        // ]
 
         // Autorise la création d'un compte administrateur
     if (req.body.username === 'admin') {
@@ -49,14 +36,7 @@ export function signup (req, res, next){
           // Empêche la création de comptes non-administrateurs
           return res.status(409).json("La création de nouveaux utilisateurs est interdite");
         }
-
-        // db.query(q, [values], (err, data)=>{
-        //     if (err) return res.json(err);
-        //     return res.status(200).json("Utilisateur créé");
-        // })
         });
-    // }
-    // });
 };
 
 export function login (req, res, next){
