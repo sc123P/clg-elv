0038c7d70781efda0a990de2bf9e6495af3676af
Simon <simon.casorla@outlook.fr>
2024-01-24 09:41:31 -0400

    changing signup in auth controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -5,29 +5,58 @@ import jwt from "jsonwebtoken";
 export function signup (req, res, next){
 
     //CHECK EXISTING USER
-    const q = "SELECT * FROM elv.user WHERE email = ? OR username = ? "
+    // const q = "SELECT * FROM elv.user WHERE email = ? OR username = ? "
+    const checkExistingAdminQuery = "SELECT * FROM elv.user WHERE isAdmin = 1";
 
-    db.query(q,[req.body.email, req.body.username], (err, data)=>{
+    // db.query(q,[req.body.email, req.body.username], (err, data)=>{
+        db.query(checkExistingAdminQuery, (err, data) => {
         //bcrypt.hash(req.body.password, 10)
 
         if(err) return res.json(err)
-        if(data.length) return res.status(409).json("Cet utilisateur existe déjà");
+        // if(data.length) return res.status(409).json("Cet utilisateur existe déjà");
+    
+        // Si un administrateur existe déjà, empêche la création d'un autre compte
+        if (data.length > 0) {
+            return res.status(409).json("Création de nouveaux utilisateurs interdit");
+        }
 
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
-        const q = "INSERT INTO user(`username`, `email`, `password`) VALUES (?)"
-        const values = [
-            req.body.username,
-            req.body.email,
-            hash,
-        ]
+        // const q = "INSERT INTO user(`username`, `email`, `password`, `isAdmin`) VALUES (?)"
+        // const values = [
+        //     req.body.username,
+        //     req.body.email,
+        //     hash,
+        //     true,
+        // ]
 
-        db.query(q, [values], (err, data)=>{
+        // Autorise la création d'un compte administrateur
+    if (req.body.username === 'admin') {
+        const insertUserQuery = "INSERT INTO user(`username`, `email`, `password`, `isAdmin`) VALUES (?)";
+        const values = [
+          req.body.username,
+          req.body.email,
+          hash,
+          true, // Le compte est administrateur
+        ];
+
+        db.query(insertUserQuery, [values], (err, data) => {
             if (err) return res.json(err);
-            return res.status(200).json("Utilisateur créé");
-        })
-    });
+            return res.status(200).json("Compte administrateur créé");
+          });
+        } else {
+          // Empêche la création de comptes non-administrateurs
+          return res.status(409).json("La création de nouveaux utilisateurs est interdite");
+        }
+
+        // db.query(q, [values], (err, data)=>{
+        //     if (err) return res.json(err);
+        //     return res.status(200).json("Utilisateur créé");
+        // })
+        });
+    // }
+    // });
 };
 
 export function login (req, res, next){
